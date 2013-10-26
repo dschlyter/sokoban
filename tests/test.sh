e4aef4406b5bb5b285aaf7c36759f24fc61b1e8c
David Schlyter <dschlyter@gmail.com>
2013-10-26 14:35:36 +0200

    Adds automated test script

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SUCCESS=0
+FAIL=0
 
 for file in "$DIR/"*.in; do
     echo $file
-    cat $file | "$DIR/../bin/iss"
+    cat $file | timeout 11 "$DIR/../bin/iss"
+
+    if [ $? -eq 0 ]; then 
+        $((SUCCESS += 1))
+    else
+        echo " --- Time Limit Exceeded --- "
+        $((FAIL += 1))
+    fi
+
+    echo Successful maps: $SUCCESS Failed maps: $FAIL
 done
